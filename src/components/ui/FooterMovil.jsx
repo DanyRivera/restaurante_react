a52ce351152f5e9d82fa646d2a6a0fef7a9cde77
Dany <dany@gmail.com>
2021-10-08 18:52:19 -0500

    Terminando v1.5

--- src/components/ui/FooterMovil.jsx
+++ src/components/ui/FooterMovil.jsx
@@ -13,18 +13,22 @@ const Footer = styled.div`
         font-size: 1.8rem;
     }
 
+    a {
+        color: #fff;
+    }
+
     span {
         font-weight: bold;
     }
 `;
 
 const FooterMovil = () => {
-    return ( 
+    return (
         <Footer>
-            <p><span>Tel: </span>222 368 8163</p>
-            <p><span>WhatsApp: </span>222 125 6696</p>
+            <p><span>Tel:</span> <a href="tel:+52-222-368-8163">222 368 8163</a></p>
+            <p><span>WhatsApp: </span><a target="_blank" rel="noreferrer" href="https://api.whatsapp.com/send?phone=522221256696&app=facebook&entry_point=page_cta&fbclid=IwAR3NeCT82q99Pe8Tb-dFVsNCHPJb5Ij_03iFsO5S8Ce6SnyBU-u0KzDzyIw">222 125 6696</a></p>
         </Footer>
     );
 }
- 
+
 export default FooterMovil;
